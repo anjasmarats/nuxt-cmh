097486d9b659e4467413d0efbc0f8966ef64b513
anjasmarats <atsakunprogramming@gmail.com>
2025-10-23 21:02:33 +0700

    Create Navbar.vue

---
+++ app/components/Navbar.vue
@@ -0,0 +1,27 @@
+<!-- Header & Navigation -->
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <span class="brand-icon">🌿</span>
+                <span>Coach Inne</span>
+            </a>
+            <button class="navbar-toggler" type="button" aria-label="Menu Navigasi Utama" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.html">Beranda</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.html">Tentang</a></li>
+                    <li class="nav-item"><a class="nav-link" href="methodology.html">Metodologi & Pendekatan</a></li>
+                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://blog.jiwasehat.com">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://tiktok.com/@coachinne579">Kontak</a></li>
+                </ul>
+                <div class="d-flex ms-3">
+                    <button class="btn btn-sm mode-toggle" id="darkModeToggle" aria-label="Toggle dark mode">
+                        <span class="light-icon">☀️</span>
+                        <span class="dark-icon">🌙</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
